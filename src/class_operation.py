d9ed9356fc50a14a155249c3d1edd588019ae136
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-06-15 14:55:22 +0300

    add meaning_to in class_operation.py

--- src/class_operation.py
+++ src/class_operation.py
@@ -23,6 +23,9 @@ class Operation:
         return self.dict_operation['description']
 
     def meaning_from(self):
+        """
+        возвращает поле ОТКУДА в отформатированном варианте
+        """
         if 'from' in self.dict_operation:
             title_from = self.dict_operation['from']
             digit_str = []
@@ -41,9 +44,33 @@ class Operation:
 
             str_not_digit = ''.join([i for i in self.dict_operation['from'] if not i.isdigit()])
             if 'счет' in title_from.lower():
-                return f"{str_not_digit}{''.join(digit_str[-4:])} ->"
+                return f"{str_not_digit}{''.join(digit_str[-7:]).replace(' ', '')} -> "
             else:
-                return f"{str_not_digit} {''.join(digit_str[0:])}"
+                return f"{str_not_digit} {''.join(digit_str[0:])} -> "
 
         else:
             return ""
+
+    def meaning_to(self):
+        """
+        возвращает поле КУДА в отформатированном варианте
+        """
+        title_to = self.dict_operation['to']
+        digit_str = []
+        sum_digit_in_str = sum(1 for i in title_to if i.isdigit())
+        for i in title_to:
+            if i.isdigit():
+                if len(digit_str) > 6 and len(digit_str) < (sum_digit_in_str - 4 + digit_str.count(' ')):
+                    digit_str.append('*')
+                    if len(digit_str) % (4) == (0 + digit_str.count(' ')) and len(digit_str) > 0:
+                        digit_str.append(' ')
+                else:
+                    digit_str.append(i)
+                    if len(digit_str) % (4) == (0 + digit_str.count(' ')) and 0 < len(digit_str) < sum_digit_in_str:
+                        digit_str.append(' ')
+        str_not_digit = ''.join([i for i in self.dict_operation['to'] if not i.isdigit()])
+        if 'счет' in title_to.lower():
+            return f"{str_not_digit}{''.join(digit_str[-7:]).replace(' ', '')}"
+        else:
+            return f"{str_not_digit} {''.join(digit_str[0:])}"
+
