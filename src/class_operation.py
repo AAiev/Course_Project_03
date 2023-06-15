bebd4ba1f34ea1e796ca0387cbe470d11a888900
Aleksandr Ievlev <a.a.ievlev@gmail.com>
2023-06-15 12:58:42 +0300

    fix class_operation.py

--- src/class_operation.py
+++ src/class_operation.py
@@ -1,21 +1,21 @@
 class Operation:
-    def __init__(self, list_operation):
-        self.list_operation = list_operation if list_operation else {}
+    def __init__(self, dict_operation):
+        self.dict_operation = dict_operation if dict_operation else {}
 
     def __repr__(self):
-        return f"Operation(dict_operation={self.list_operation})"
+        return f"Operation(dict_operation={self.dict_operation})"
 
 
-    def date_operation(self, dict_operation):
+    def date_operation(self):
         """
         date_operation - из полученного словаря получает строку по ключу 'date'
         и возвращает дату в формате: ДД.ММ.ГГГГ
         :param dict_operation: словарь операции
         :return: дата в формате ДД.ММ.ГГГГ
         """
-        date_operation_year = dict_operation['date'][0:4]
-        date_operation_month = dict_operation['date'][5:7]
-        date_operation_day = dict_operation['date'][8:10]
+        date_operation_year = self.dict_operation['date'][0:4]
+        date_operation_month = self.dict_operation['date'][5:7]
+        date_operation_day = self.dict_operation['date'][8:10]
         return f'{date_operation_day}.{date_operation_month}.{date_operation_year}'
 
 
